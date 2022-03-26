9640abd3be84f8857a99601bf7e7f2827338233c
Marcin Mielnicki <s23891@pjwstk.edu.pl>
2022-03-26 22:31:43 +0100

    Zadanie 4

---
+++ src/CW1/zad4/zad4.java
@@ -0,0 +1,23 @@
+package CW1.zad4;
+
+public class zad4 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while(i < 10) {
+            System.out.println( "Hello World!" );
+            i++;
+        }
+
+        i = 0;
+        do {
+            System.out.println( "Hello World!");
+            i++;
+        }
+        while(i < 10);
+
+        for(i = 0; i < 10; i++) {
+            System.out.println( "Hello World!");
+        }
+    }
+}
