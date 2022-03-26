6f42470465794e5f03de038f45f1a1534e80685e
Marcin Mielnicki <s23891@pjwstk.edu.pl>
2022-03-26 22:31:22 +0100

    Zadanie 2

--- src/CW1/zad2/zad2.java
+++ src/CW1/zad2/zad2.java
@@ -1,19 +1,39 @@
 package CW1.zad2;
 
+import java.util.Scanner;
+
 public class zad2 {
     public static void main(String[] args) {
+        System.out.println( "Wpisz 3 liczby po spacji" );
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt(),
-        b = scanner.nextInt(),
-        c = scanner.nextInt();
-        System.out.println( a + " " + b + " " + c);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-        if(a > b && a > c)
+        int temp = 0;
+
+        if ( a > b)
         {
-        int tmp = c;
-        c = a;
-        a = tmp;
+            temp = a;
+            a = b;
+            b = temp;
         }
+
+        if (c < a)
+        {
+            temp = c;
+            c = b;
+            b = a;
+            a = temp;
+        }
+        else if (c < b)
+        {
+            temp = b;
+            b = c;
+            c = temp;
+        }
+
+        System.out.println( a + " " + b + " " + c);
     }
 }
